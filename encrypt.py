3ab1389513dc994644df0a11e6ebcf06a078b4a2
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-04-14 16:23:43 +0200

    Fix encryption

--- encrypt.py
+++ encrypt.py
@@ -4,17 +4,19 @@ import unidecode
 
 
 n = int(sys.argv[1])
-message = sys.argv[2:]
-message = message[0]
-print(message)
+message = sys.argv[2]
 message = message.upper()
 
-message_crypt = []
+cipher = ""
 
 for character in message:
-  if character in string.ascii_uppercase:
-    message_crypt += [chr((ord(character) + n) % 26)]
+    if character in string.ascii_uppercase:
+        ascii_code = ord(character)
+        alpha_index = ascii_code - ord("A")  # between 0 and 25
+        alpha_index = (alpha_index + n) % 26  # between 0 and 25
+        encrypted_ascii_code = ord("A") + alpha_index
+        encrypted_character = chr(encrypted_ascii_code)
+        cipher += encrypted_character
 
-message_crypt = "".join(message_crypt)
 
-print(message_crypt)
+print(cipher)
