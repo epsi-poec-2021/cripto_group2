bcb9ecded4a452e330a59fe47d9d7d99aee75865
Bangaly Cisse <bangaly.cisse@epsi.fr>
2021-04-15 20:15:00 +0200

    add encrypt-v2.py

---
+++ encrypt-v2.py
@@ -0,0 +1,15 @@
+import sys
+key = sys.argv[1].upper()
+message = sys.argv[2].upper()
+encrypted_message = ""
+coefficient = len(message) // len(key)
+number_character_remaining = len(message) % len(key)
+key_good_length = coefficient * key + key[:number_character_remaining]
+message_size = len(message)
+for i in range(message_size):
+	shift = ord(key_good_length[i]) % ord('A') 
+	encrypted_index_letter =  ord(message[i])  + shift
+	encrypted_character = chr(encrypted_index_letter) 
+	encrypted_message += encrypted_character
+
+print(encrypted_message) 
