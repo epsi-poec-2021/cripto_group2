4677f116f89490acd061da28752d435e5c44a089
Bangaly Cisse <bangaly.cisse@epsi.fr>
2021-04-16 08:16:51 +0200

    improvements :be in the  interval [ord ('A'), ord ('Z')] after adding or substracting the shift value; algorithmes works also for sentences.

--- decrypt-v2.py
+++ decrypt-v2.py
@@ -1,15 +1,47 @@
 import sys
-key = sys.argv[1].upper()
-message = sys.argv[2].upper()
-encrypted_message = ""
-coefficient = len(message) // len(key)
-number_character_remaining = len(message) % len(key)
-key_good_length = coefficient * key + key[:number_character_remaining]
-message_size = len(message)
-for i in range(message_size):
-	shift = ord(key_good_length[i]) % ord('A') 
-	encrypted_index_letter =  ord(message[i])  - shift # only this line changed in comparison to encrypt-v2.py
-	encrypted_character = chr(encrypted_index_letter) 
-	encrypted_message += encrypted_character
+import string
 
-print(encrypted_message) 
+def delete_space_punctuation(message):
+	message_without_punctuation = ""
+	for character in message:
+		if character in string.ascii_uppercase:
+
+			message_without_punctuation += character
+
+	return message_without_punctuation
+
+def create_key_length_equal_to(message, key):
+	coefficient = len(message) // len(key)
+	number_character_remaining = len(message) % len(key)
+	key_good_length = coefficient * key + key[:number_character_remaining]
+	
+	return key_good_length
+
+def decrypt(message, key_good_length):
+	message_size = len(message)
+	decrypted_message = ""
+	for i in range(message_size):
+		shift = ord(key_good_length[i]) % ord('A') 
+		decrypted_index_letter =  ord(message[i])  - shift 
+		if decrypted_index_letter < ord('A'):
+			#decrypted_index_letter can't be inferior to encrypted_index_letter - 25 (shift in beetween 0 at 25) 
+			#Thus we don't need to add ord(Z) more than one time
+			decrypted_index_letter = decrypted_index_letter - ord('A') + ord('Z') + 1 
+		decrypted_character = chr(decrypted_index_letter) 
+		decrypted_message += decrypted_character
+
+	return decrypted_message
+
+def main():
+	key = sys.argv[1].upper()
+	message = sys.argv[2].upper()
+	message = delete_space_punctuation(message)
+	key_good_length = create_key_length_equal_to(message, key)
+	decrypted_message = decrypt(message, key_good_length)
+	print(decrypted_message)
+
+
+main()
+
+
+
--- encrypt-v2.py
+++ encrypt-v2.py
@@ -1,15 +1,42 @@
 import sys
-key = sys.argv[1].upper()
-message = sys.argv[2].upper()
-encrypted_message = ""
-coefficient = len(message) // len(key)
-number_character_remaining = len(message) % len(key)
-key_good_length = coefficient * key + key[:number_character_remaining]
-message_size = len(message)
-for i in range(message_size):
-	shift = ord(key_good_length[i]) % ord('A') 
-	encrypted_index_letter =  ord(message[i])  + shift 
-	encrypted_character = chr(encrypted_index_letter) 
-	encrypted_message += encrypted_character
+import string
 
-print(encrypted_message) 
+def delete_space_punctuation(message):
+	message_without_punctuation = ""
+	for character in message:
+		if character in string.ascii_uppercase:
+
+			message_without_punctuation += character
+
+	return message_without_punctuation
+
+def create_key_length_equal_to(message, key):
+	coefficient = len(message) // len(key)
+	number_character_remaining = len(message) % len(key)
+	key_good_length = coefficient * key + key[:number_character_remaining]
+	
+	return key_good_length
+
+def encrypt(message, key_good_length):
+	message_size = len(message)
+	encrypted_message = ""
+	for i in range(message_size):
+		shift = ord(key_good_length[i]) % ord('A') 
+		encrypted_index_letter =  ord(message[i])  + shift 
+		if encrypted_index_letter > ord('Z'):
+			encrypted_index_letter = encrypted_index_letter % ord('Z') + ord('A') - 1
+		encrypted_character = chr(encrypted_index_letter) 
+		encrypted_message += encrypted_character
+
+	return encrypted_message
+
+def main():
+	key = sys.argv[1].upper()
+	message = sys.argv[2].upper()
+	message = delete_space_punctuation(message)
+	key_good_length = create_key_length_equal_to(message, key)
+	encrypted_message = encrypt(message, key_good_length)
+	print(encrypted_message)
+
+
+main()
